LineageOS/android_device_htc_m8d

m8d: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_m8d.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_m8d-user \
+    lineage_m8d-userdebug \
+    lineage_m8d-eng

AICP/device_htc_m8d

AndroidProducts.mk
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aicp_m8d.mk
+
+COMMON_LUNCH_CHOICES := \
+    aicp_m8d-user \
+    aicp_m8d-userdebug \
+    aicp_m8d-eng